ae31123a0a79e75c5de7af7e8a52416727acb196
土屋　夏実 <t.natsu3@icloud.com>
2024-10-18 15:33:23 +0900

    Create Money.java

---
+++ Money.java
@@ -0,0 +1,22 @@
+//おつりの枚数判定システム
+//500円，100円，50円，10円
+
+import java.util.Scanner;
+public class Money {
+    public static void main (String[] args){ 
+        int n, x, money;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("金額xを入力してください>>");
+        x= sc.nextInt();
+        System.out.print("支払い金額nを入力してください>>");
+        n= sc.nextInt();
+
+        money= n- x;  
+        System.out.println("おつりは"+ money+ "円です");
+
+        System.out.println("500円玉の枚数は"+  money/ 500+ "枚です");
+        System.out.println("100円玉の枚数は"+ (money% 500)/ 100+ "枚です");
+        System.out.println(" 50円玉の枚数は"+ (money% 500 % 100)/ 50+ "枚です");
+        System.out.println(" 10円玉の枚数は"+ (money% 500 % 100%  10)/ 10+ "枚です");
+    }
+}
